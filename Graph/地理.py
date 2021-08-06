fca8d4546dccea28c5b5b756233e2e31355a9f71
uuu321 <1003664160@qq.com>
2021-08-06 14:55:20 +0800

    Graph First Commit

---
+++ Graph/地理.py
@@ -0,0 +1,52 @@
+from py2neo import Graph, Node, Relationship, NodeMatcher, RelationshipMatcher
+import xlrd
+import re
+from aip import AipNlp
+
+APP_ID = '19052765'
+API_KEY = 'uToZ2OK0hkXXXlkXFLgu9lV1'
+SECRET_KEY = '3XOQxlI5X4S2jKCs1aQ5hS3Vu7EShpdq'
+client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
+# 数据库
+graph = Graph("http://localhost:7474", auth=("neo4j", "123456"))
+
+matcher = NodeMatcher(graph)
+relMatcher = RelationshipMatcher(graph)
+
+# xlsx路径
+path = '/Users/fuuujy/Desktop/三常知识图谱/地理三元组.xlsx'
+data = xlrd.open_workbook(path)  # 打开Excel文件读取数据
+table = data.sheets()[0]  # 通过索引顺序获取
+# xlsx循环
+for j in range(1, table.nrows):  # 循环行列表数据
+    # 一级标题
+    title1 = str(table.row(j)[0].value)  # 使用行索引
+    # 二级标题
+    title2 = str(table.row(j)[1].value)
+    # 节点1
+    title3 = str(table.row(j)[5].value)
+    print("title3=", title3)
+    node_a = matcher.match(title3, name=title3, source1='地理', source2=title1, source3=title2).first()
+    if not node_a:
+        node_a = Node(title3, name=title3, source1='地理', source2=title1, source3=title2)
+        graph.create(node_a)
+        print(node_a)
+    # 关系
+    title4 = str(table.row(j)[6].value)
+    print("title4=", title4)
+    # 节点2
+    title5 = str(table.row(j)[7].value)
+    print("title5=", title5)
+    node_c = matcher.match(title5, name=title5, source1='地理', source2=title1, source3=title2).first()
+    if not node_c:
+        node_c = Node(title5, name=title5, source1='地理', source2=title1, source3=title2)
+        graph.create(node_c)
+        print(node_c)
+    # 节点1-（关系）->节点2
+    rel = relMatcher.match((node_a, node_c), r_type=title4).first()
+    if rel == None:
+        rel = Relationship(node_a, title4, node_c)
+        graph.create(rel)
+        print(rel)
+
+print('Finish')

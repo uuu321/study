5bfd3b47ce19fbfe49dfec88c472fc4907268d17
fujingyu <1003664160@qq.com>
2021-12-20 18:14:59 +0800

    mixins study

---
+++ vueStudy/study01/src/components/mixinsUse.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "mixinsUse"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vueStudy/study01/src/components/mixinsUse02.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "mixinsUse02"
+}
+</script>
+
+<style scoped>
+
+</style>
